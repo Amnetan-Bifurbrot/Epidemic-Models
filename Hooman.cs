e81eeba0dbd6b89dfa426956d6a82ba8cffa7e3e
Adam Wiśniewski <adamwisiwmada@gmail.com>
2020-04-24 20:03:59 +0200

    coś się znowu zepsuło

--- Hooman.cs
+++ Hooman.cs
@@ -3,7 +3,7 @@
 namespace Epidemic_Models {
 	class Hooman {
 		public bool isSusceptible, isInfected, isRecovered, isVaccinated;
-		int recoveryCounter = 0, contactCounter = 0;
+		int recoveryCounter = 0;
 		public static Graph<Hooman> society = new Graph<Hooman>(false, false);
 		public static double[][] graphdata = new double[5][];
 
@@ -15,10 +15,8 @@ namespace Epidemic_Models {
 		}
 
 		public static void SpreadDisease(double beta, double gamma, double lambda, double mu, double xi, int howLong, int maxDegree) {
-			int contactTime = (int)(1 / beta), recoveryTime = (int)(1 / gamma), sAmount = 0, iAmount = 0, rAmount = 0, vAmount = 0;
+			int recoveryTime = (int)(1 / gamma), sAmount = 0, iAmount = 0, rAmount = 0, vAmount = 0;
 			Random rand = new Random();
-			int birthCounter = 0, deathCounter = 0, maxBirth = 1, maxDeath = 1, stopBirthCounter = 0, stopDeathCounter = 0, length_b = 0, length_d = 0;
-			bool birth = false, death = false;
 			for (int i = 0; i < 5; i++)
 				graphdata[i] = new double[howLong];
 
@@ -28,82 +26,8 @@ namespace Epidemic_Models {
 				rAmount = 0;
 				vAmount = 0;
 
-				if (stopBirthCounter == (int)((Math.Pow(10, length_b))/maxBirth)) {
-					birth = false;
-					stopBirthCounter = 0;
-					birthCounter = 0;
-				}
-
-				if (birth == false) {
-					string maxBirth_ = Convert.ToString(lambda);
-					length_b = maxBirth_.Length - 2;
-					maxBirth = (int)(lambda * Math.Pow(10, length_b));
-					birth = true;
-				}
-
-	
-				if (birth == true) {				
-					if (birthCounter < 1) {
-						//rodzimy się i ewentualnie szczepimy
-						if (rand.NextDouble() < xi)
-							society.AddNodeWithRandomEdges(maxDegree, new Hooman(false, false, false, true));        //narodziny + szczepienie
-						else
-							society.AddNodeWithRandomEdges(maxDegree, new Hooman());                             // narodziny bez szczepienia
-						birthCounter++;
-					}
-					stopBirthCounter++;
-				}
-				
-				//Console.WriteLine("stopbirthCounter: " + stopBirthCounter);
-				//Console.WriteLine();
-
-				foreach (Node<Hooman> hooman in society.Nodes) {
-					if (hooman.Data.isInfected) {                               //rozprzestrzeniamy choróbska
-						hooman.Data.contactCounter++;
-						if (hooman.Data.contactCounter > contactTime) {
-							hooman.Data.contactCounter = 0;
-							foreach (Node<Hooman> neighbor in hooman.Neighbours) {
-								if (neighbor.Data.isSusceptible) {
-									neighbor.Data.isSusceptible = false;
-									neighbor.Data.isInfected = true;
-								}
-							}
-						}
-						hooman.Data.recoveryCounter++;
-						if (hooman.Data.recoveryCounter > recoveryTime) {       //i wychodzimy z nich zdrowi (czasem)
-							hooman.Data.isInfected = false;
-							hooman.Data.isRecovered = true;
-						}
-					}
-				}
-
-				if (stopDeathCounter == (int)((Math.Pow(10, length_d)/maxDeath))) {
-					death = false;
-					stopDeathCounter = 0;
-					deathCounter = 0;
-				}
-
-				if (death == false) {
-					string maxDeath_ = Convert.ToString(mu);
-					length_d = maxDeath_.Length - 2;
-					maxDeath = (int)(mu * Math.Pow(10, length_d));
-					death = true;
-				}
-
-				if (death == true) {
-					if (deathCounter < 1) {
-						//usmiercamy
-						society.RemoveNode(society.Nodes[rand.Next(0, society.Nodes.Count)]);
-						deathCounter++;
-					}
-					stopDeathCounter++;
-				}
-				
-				//Console.WriteLine("stopdeathCounter: " + stopDeathCounter);
-				//Console.WriteLine();
-
-				//policzmy ich
-				foreach (Node<Hooman> hooman in society.Nodes) {
+				foreach(Node<Hooman> hooman in society.Nodes) {
+					//liczenie
 					if (hooman.Data.isSusceptible)
 						sAmount++;
 					else if (hooman.Data.isInfected)
@@ -112,6 +36,22 @@ namespace Epidemic_Models {
 						rAmount++;
 					else
 						vAmount++;
+
+					if (hooman.Data.isSusceptible) {
+						foreach(Node<Hooman> neighbor in hooman.Neighbours) {
+							if (neighbor.Data.isInfected && rand.NextDouble() < beta / hooman.Neighbours.Count) {
+								hooman.Data.isSusceptible = false;
+								hooman.Data.isInfected = true;
+								break;
+							}
+						}
+					} else if (hooman.Data.isInfected) {
+						if (hooman.Data.recoveryCounter > recoveryTime) {
+							hooman.Data.isInfected = false;
+							hooman.Data.isRecovered = true;
+						}
+						hooman.Data.recoveryCounter++;
+					}
 				}
 
 				graphdata[0][i] = i;        //czas
