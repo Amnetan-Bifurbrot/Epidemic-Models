fcd592a48f00548a39d1840b6723e53d53dc3d00
Zuzanna Wieczorek <46034524+zuza3012@users.noreply.github.com>
2020-04-25 19:19:18 +0200

    Średnia, worker, ikona, zapis wykresu

---
+++ Popup.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Epidemic_Models {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Popup : Window {
+        public Popup() {
+            InitializeComponent();
+        }
+        public event EventHandler CancelHappened;
+
+        private void Cancel_Button_Click(object sender, RoutedEventArgs e) {
+            if (CancelHappened != null) {
+                CancelHappened(this, e);
+            }          
+        }
+
+    }
+}
